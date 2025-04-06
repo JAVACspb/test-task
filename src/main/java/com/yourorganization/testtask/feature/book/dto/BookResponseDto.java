f1ed22df3421c23ac8dfc31b484fd92c036bd3dd
Aleksandr <al.alentjev@yandex.ru>
2025-04-06 18:37:14 +0300

    Feat: Add Valid for DTO

--- src/main/java/com/yourorganization/testtask/feature/book/dto/BookResponseDto.java
+++ src/main/java/com/yourorganization/testtask/feature/book/dto/BookResponseDto.java
@@ -3,16 +3,33 @@ package com.yourorganization.testtask.feature.book.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.UUID;
 @Getter
 @Setter
 public class BookResponseDto {
+    @NotNull(message = "ID is mandatory")
     private UUID id;
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
     private String title;
+
+    @NotBlank(message = "Author is mandatory")
+    @Size(min = 1, max = 255, message = "Author name must be between 1 and 255 characters")
     private String author;
+
+    @NotBlank(message = "ISBN is mandatory")
+    @Size(min = 10, max = 13, message = "ISBN must be between 10 and 13 characters")
     private String isbn;
+
+    @NotNull(message = "Creation time is mandatory")
     private LocalDateTime createdAt;
+
+    @NotNull(message = "Update time is mandatory")
     private LocalDateTime updatedAt;
 
     public BookResponseDto() {
